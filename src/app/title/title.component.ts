e40988fc7a61af6be8a27f939fa033e6f9ecc460
chnnaama <chn.naama@gmail.com>
2020-03-25 20:11:19 +0200

    add firebase - hosting & firestore

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -8,25 +8,43 @@ import { Observable } from 'rxjs';
   styleUrls: ['./title.component.scss']
 })
 export class TitleComponent implements OnInit {
-  private titles: string[];
-  private title: string;
-  items: Observable<any[]>;
+  items: string[];
+  title: string;
+  itemLeft: number;
+  session$: Observable<any>;
+  // items: Observable<any[]>;
+  // private sassion: ;
   constructor(private afs: AngularFirestore) {
-    this.items = this.afs.collection('items').valueChanges();
-    this.titles = [
-      'nadav', 'leor', 'naama', 'idan', 'aa', 'ff', '1231', '21312'
-    ];
-    this.title = 'hello';
+    // this.items = this.afs.collection('items').valueChanges();
+
+    this.session$ = this.afs.collection('sassion').doc('1').valueChanges();
   }
 
   async ngOnInit() {
-
+    this.session$.subscribe( session => {
+      this.items = session.items;
+      this.title = session.title;
+      this.itemLeft = this.items.length;
+      console.log(session);
+    });
   }
 
   onClick() {
-    const random = Math.floor(Math.random() * this.titles.length);
-    this.title = this.titles[random];
-    this.titles.splice(random, 1);
+    const random = Math.floor(Math.random() * this.items.length);
+    this.title = this.items[random];
+    // update db
+    this.items.splice(random, 1);
+    this.itemLeft = this.items.length;
+    // update db
+    this.afs.doc('sassion/1').update({title: this.title, items: this.items}).then(a => console.log(a));
+    // const ress = await admin.firestore().doc('test/' + uid).update({ttt: 't', paid: true, foo: 'bahhr', date: date});
   }
 
+  onRest() {
+    this.title = '';
+    this.items = [
+      'nadav', 'leor', 'naama', 'idan', 'ortal', 'rostam', 'imanuel', 'evyatar'
+    ];
+    this.afs.doc('sassion/1').update({title: this.title, items: this.items}).then(a => console.log(a));
+  }
 }
